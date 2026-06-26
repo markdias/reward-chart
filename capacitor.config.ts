832df6e4f5115b00bc248232c1c63fa5b0baedb7
Mark Dias <mark@mdias.co.uk>
2026-06-26 16:42:21 +0100

    feat: initialize capacitor for ios build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.markdias.rewardchart',
+  appName: 'Reward Chart',
+  webDir: 'dist'
+};
+
+export default config;
