1076e18e7f7790143e37ffb95227edb4a67aff37
Mark Dias <mark@mdias.co.uk>
2026-07-22 20:43:08 +0100

    fix: ensure onboarding setup data is saved and synced when creating an account

--- android/app/src/main/assets/public/assets/web-ch3cu1zn.js
+++
@@ -1,1 +0,0 @@
-import{W as e}from"./index-yo-YQaN0.js";class t extends e{async getStatus(){return{status:"authorized"}}async requestPermission(){return{status:"authorized"}}async getPluginVersion(){return{version:"web"}}}export{t as AppTrackingTransparencyWeb};
